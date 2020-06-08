c850a910bf24260d0ae22aca52f7c6afb5aaa4c4
jr <gagito0594@gmail.com>
2020-06-08 10:42:50 +0800

    j
    jr

--- App.js
+++ App.js
@@ -1,77 +1,20 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import LoginScreen from './src/screen/LoginScreen';
+import HomeScreen from './src/screen/HomeScreen';
+const Stack = createStackNavigator();
 
-import React from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
-
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-const App: () => React$Node = () => {
+function App() {
   return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <Text>test</Text>
-      </SafeAreaView>
-    </>
+    <NavigationContainer>
+      <Stack.Navigator>
+      {/* <Stack.Screen name="Main" component={HomeScreen} options={{ headerShown: false }}/> */}
+      <Stack.Screen name="Login" component={LoginScreen}options={{ headerShown: false }} />
+      <Stack.Screen name="Main" component={HomeScreen} options={{ headerShown: false }}/>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
-};
-
-const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
-});
+}
 
 export default App;
---
+++ src/screen/HomeScreen.js
@@ -0,0 +1,46 @@
+import React,{useState,useRef} from 'react';
+import {StyleSheet,View,Text} from "react-native";
+
+
+export default function HomeScreen () {
+
+
+return (
+  <View style={styles.container}>
+      <Text>test homescreen</Text>
+  </View>
+)
+
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#ffffff',
+  },
+  welcome: {
+    fontSize: 18,
+    color: '#222',
+    marginBottom: 16,
+  },
+  button: {
+    backgroundColor: '#34a853',
+    borderRadius: 8,
+    height: 56,
+    paddingHorizontal: 24,
+    justifyContent: 'center',
+    marginVertical: 8,
+  },
+  direct: {
+    backgroundColor: '#db7d35',
+  },
+  stripe: {
+    backgroundColor: '#556cd6',
+  },
+  buttonText: {
+    color: '#ffffff',
+    fontSize: 18,
+  },
+})
