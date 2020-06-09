bec8b7ec16a4a190a097cf263051a0c0720df7c6
jr <gagito0594@gmail.com>
2020-06-09 16:35:54 +0800

    jr
    jr

--- src/screen/ScanScreen.js
+++ src/screen/ScanScreen.js
@@ -1,43 +1,60 @@
-import React,{useState,useRef} from 'react';
-import {StyleSheet,View,Text} from "react-native";
+'use strict';
 
+import React, { Component } from 'react';
 
-export default function ScanScreen () {
-return (
-  <View style={styles.container}>
-      <Text>test scan screen</Text>
-  </View>
-)
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  Linking
+} from 'react-native';
+
+import QRCodeScanner from 'react-native-qrcode-scanner';
+import { RNCamera } from 'react-native-camera';
+
+export default function ScanScreen ({navigation}) {
+  const onSuccess = e => {
+    Linking.openURL(e.data).catch(err =>
+      console.error('An error occured', err)
+    );
+  };
+    return (
+      <QRCodeScanner
+        onRead={onSuccess}
+        flashMode={RNCamera.Constants.FlashMode.torch}
+        topContent={
+          <Text style={styles.centerText}>
+            Go to{' '}
+            <Text style={styles.textBold}>wikipedia.org/wiki/QR_code</Text> on
+            your computer and scan the QR code.
+          </Text>
+        }
+        bottomContent={
+          <TouchableOpacity style={styles.buttonTouchable}>
+            <Text style={styles.buttonText}>OK. Got it!</Text>
+          </TouchableOpacity>
+        }
+      />
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  centerText: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#ffffff',
-  },
-  welcome: {
     fontSize: 18,
-    color: '#222',
-    marginBottom: 16,
+    padding: 32,
+    color: '#777'
   },
-  button: {
-    backgroundColor: '#34a853',
-    borderRadius: 8,
-    height: 56,
-    paddingHorizontal: 24,
-    justifyContent: 'center',
-    marginVertical: 8,
-  },
-  direct: {
-    backgroundColor: '#db7d35',
-  },
-  stripe: {
-    backgroundColor: '#556cd6',
+  textBold: {
+    fontWeight: '500',
+    color: '#000'
   },
   buttonText: {
-    color: '#ffffff',
-    fontSize: 18,
+    fontSize: 21,
+    color: 'rgb(0,122,255)'
   },
-})
+  buttonTouchable: {
+    padding: 16
+  }
+});
